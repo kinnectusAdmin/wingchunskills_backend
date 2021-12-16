9c0ffc0c86dfaa8e0248b34a99bd627aec3e4715
Blake Rogers <blake.rogers@SKOOKUM-C02XK4X8JG5L.lan>
2021-12-16 16:41:15 -0500

    manuallly create user

--- server/models/seedSchema.js
+++ server/models/seedSchema.js
@@ -1,3 +1,30 @@
-require('./Post');
-require('./User');
-require('../database').sync();
+const { DataTypes, Sequelize } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+const User = sequelize.define('User', {
+  // Model attributes are defined here
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  resetPasswordToken: {
+    type: Sequelize.STRING
+  },
+  resetPasswordExpires: {
+    type: Sequelize.DATE
+  }
+}, {
+  // Other model options go here
+});
+
+await sequelize.sync({ force: true });
+console.log("All models were synchronized successfully.");
+// `sequelize.define` also returns the model
+console.log(User === sequelize.models.User)

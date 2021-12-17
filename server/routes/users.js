8103a065b4d82c9e57b146fdb280a615241e4862
Blake Rogers <blake.rogers@SKOOKUM-C02XK4X8JG5L.lan>
2021-12-16 22:23:07 -0500

    bypass login logic

--- server/routes/users.js
+++ server/routes/users.js
@@ -23,61 +23,65 @@ router.post(
     check('password', 'Password is required').exists()
   ],
   async (req, res) => {
-    console.log(req.body);
-
-    const errors = validationResult(req);
-    console.log(errors.array());
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    const { email, password } = req.body;
-    try {
-      let user = await User.findAll({ where: { email: email.toLowerCase() } });
-      //   let user = await User.findOne({ email: email.toLowerCase() });
-      console.log(user);
-      if (!user) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: 'Invalid Credentials' }] });
-      }
-
-      const isMatch = await bcrypt.compare(
-        password,
-        user[0].dataValues.password
-      );
-
-      if (!isMatch) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: 'Invalid Credentials' }] });
-      }
-
-      const payload = {
-        user: {
-          id: user[0].dataValues.id,
-          name: user[0].dataValues.name,
-          email: user[0].dataValues.email
-        }
-      };
-
-      console.log(payload.user);
-
-      jwt.sign(
-        payload,
-        process.env.jwtToken,
-        { expiresIn: 3600 },
-        (err, token) => {
-          if (err) throw err;
-          let now = new Date();
-          now.setTime(now.getTime() + 3600000);
-          let expires = now.toUTCString();
-          let cookie =
-            'token=' + token + '; Expires=' + expires + '; Path=/; HttpOnly';
-          res.setHeader('Set-Cookie', cookie);
-          res.send(JSON.stringify({ token, user: payload.user, expires }));
-        }
-      );
+    let now = new Date();
+    now.setTime(now.getTime() + 3600000);
+    let expires = now.toUTCString();
+    res.send(JSON.stringify({ "token", user: {name: "test"}, expires }));
+    // console.log(req.body);
+    //
+    // const errors = validationResult(req);
+    // console.log(errors.array());
+    // if (!errors.isEmpty()) {
+    //   return res.status(400).json({ errors: errors.array() });
+    // }
+    //
+    // const { email, password } = req.body;
+    // try {
+    //   let user = await User.findAll({ where: { email: email.toLowerCase() } });
+    //   //   let user = await User.findOne({ email: email.toLowerCase() });
+    //   console.log(user);
+    //   if (!user) {
+    //     return res
+    //       .status(400)
+    //       .json({ errors: [{ msg: 'Invalid Credentials' }] });
+    //   }
+    //
+    //   const isMatch = await bcrypt.compare(
+    //     password,
+    //     user[0].dataValues.password
+    //   );
+    //
+    //   if (!isMatch) {
+    //     return res
+    //       .status(400)
+    //       .json({ errors: [{ msg: 'Invalid Credentials' }] });
+    //   }
+    //
+    //   const payload = {
+    //     user: {
+    //       id: user[0].dataValues.id,
+    //       name: user[0].dataValues.name,
+    //       email: user[0].dataValues.email
+    //     }
+    //   };
+    //
+    //   console.log(payload.user);
+    //
+    //   jwt.sign(
+    //     payload,
+    //     process.env.jwtToken,
+    //     { expiresIn: 3600 },
+    //     (err, token) => {
+    //       if (err) throw err;
+    //       let now = new Date();
+    //       now.setTime(now.getTime() + 3600000);
+    //       let expires = now.toUTCString();
+    //       let cookie =
+    //         'token=' + token + '; Expires=' + expires + '; Path=/; HttpOnly';
+    //       res.setHeader('Set-Cookie', cookie);
+    //       res.send(JSON.stringify({ token, user: payload.user, expires }));
+    //     }
+    //   );
     } catch (err) {
       console.error(err.message);
       console.log('nope!');

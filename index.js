9f84e4fd215688754ab7fa8396ce032124e591d8
Blake Rogers <blake.rogers@SKOOKUM-C02XK4X8JG5L.lan>
2021-12-16 22:17:03 -0500

    reorganize index

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 process.env.NODE_ENV != 'production' ? require('dotenv').config() : null;
 const express = require('express');
 const app = express();
+const posts = require('./server/routes/posts');
+const users = require('./server/routes/users');
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
@@ -11,10 +13,6 @@ app.use(cookieParser());
 
 require('./server/database');
 
-const posts = require('./server/routes/posts');
-app.use('/posts', posts);
-const users = require('./server/routes/users');
-app.use('/users', users);
 
 const port = process.env.PORT || 3000;
 
@@ -25,6 +23,10 @@ app.get('/admin', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(__dirname + '/404.html');
 });
+
+app.use('/posts', posts);
+
+app.use('/users', users);
 
 app.listen(port, () => {
   console.log('Server started on port ' + port);

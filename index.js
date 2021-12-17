2dea8ce2306fd34186663aa61d84f7ea276f6b23
Blake Rogers <blake.rogers@SKOOKUM-C02XK4X8JG5L.lan>
2021-12-17 12:01:06 -0500

    only use users route

--- index.js
+++ index.js
@@ -6,31 +6,31 @@ const users = require('./server/routes/users');
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+// var bodyParser = require('body-parser');
+// var cookieParser = require('cookie-parser');
+//
+// app.use(cookieParser());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
 
 
 const port = process.env.PORT || 3000;
 
-app.get('/admin', (req, res) => {
-  res.sendFile(__dirname + '/login.html');
-});
-app.use('/posts', posts);
+// app.get('/admin', (req, res) => {
+//   res.sendFile(__dirname + '/login.html');
+// });
+// app.use('/posts', posts);
 
 app.use('/users', users);
 
-app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/404.html');
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(__dirname + '/404.html');
+// });
 
 
-require('./server/database');
+// require('./server/database');
 
 app.listen(port, () => {
   console.log('Server started on port ' + port);
